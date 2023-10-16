bc91f9211260f09707026cebb13abfdfee5bcce9
DiyamAlr <Deemrabah99@gmail.com>
2023-10-16 12:52:31 +0300

    added the badges collction screen

--- Teshames/Screens/BadgesCollectionScreen.swift
+++ Teshames/Screens/BadgesCollectionScreen.swift
@@ -9,87 +9,116 @@ import SwiftUI
 
 struct BadgesCollectionScreen: View {
     var body: some View {
-        
         ScrollView{
+            
+            let months = ["Nov","Dec","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct ","Nov","Dec "]
             VStack{
                 VStack{
                     HStack(alignment: .top ) {
                         VStack(alignment: .leading, spacing: 12 ) {
-                            Text("Oct  ")
+                            Text("Oct")
                                 .font(.title2)
                                 .fontWeight(.semibold)
                                 .foregroundColor(Color.black)
                                 .multilineTextAlignment(.leading)
-                                .padding(.leading)
+                                .padding(.leading, 35.0)
                         }
-
+                        
                         Spacer()
-
-
-                    }.padding(.bottom,9)
-                    VStack{
-                        HStack(){
-                            Text("Welcome ")
-                            Text("Welcome ")
-                            Text("Welcome ")
-
-                        }
-                    }
-                      
-                    }.frame(width: 370,height: 120)
-                        .background(Color.background)
-                        .cornerRadius(20)
-                        .toolbar {
-                            ToolbarItem(placement: .principal) {
-                                HStack {
-                                   
-                                    Text("Badges").font(.headline)
-                                }
-                            }
-                            
-                        }
-                     
+                        
+                        
+                    }.padding(.bottom,9)}
                 VStack{
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading, spacing: 12 ) {
-                            Text("Oct ")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                                .foregroundColor(Color.black)
-                                .multilineTextAlignment(.leading)
-                                .padding(.leading)
+                    HStack(){
+                        VStack{ Image("Badge 2")
+                                .resizable()
+
+                                .padding(/*@START_MENU_TOKEN@*/[.top, .leading]/*@END_MENU_TOKEN@*/)
+                                .scaledToFit()
+                            Text("1 Day")
                         }
-
-                        Spacer()
-
-
-                    }.padding(.bottom,9)
-                    VStack{
-                        HStack(){
-                            Text("Welcome ")
-                            Text("Welcome ")
-                            Text("Welcome ")
-
+                        VStack{ Image("Badge 5")
+                                .resizable()
+            
+                                .padding(/*@START_MENU_TOKEN@*/[.top, .trailing]/*@END_MENU_TOKEN@*/)
+                                .scaledToFit()
+                                .opacity(0.3)
+                            Text("1 Week")
+                        }
+                        
+                        VStack{     Image("Badge 6")
+                                .resizable()
+                             
+                                .padding([.top, .trailing])
+                                .scaledToFit()
+                                .opacity(0.3)
+                            Text("1 Month")
                         }
                     }
-                      
-                    }.frame(width: 370,height: 120)
-                        .background(Color.background)
-                        .cornerRadius(20)
-                     
-                
-                
-            }
-            
-            //2
-            
-
+                    Divider()
                     
+                    ForEach (months, id: \.self){ month in
+                        VStack{
+                            VStack{
+                                HStack(alignment: .top ) {
+                                    VStack(alignment: .leading, spacing: 12 ) {
+                                        Text(month)
+                                            .font(.title2)
+                                            .fontWeight(.semibold)
+                                            .foregroundColor(Color.black)
+                                            .multilineTextAlignment(.leading)
+                                            .padding(.leading)
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    
+                                }.padding(.bottom,9)
+                                VStack{
+                                    HStack(){
+                                        VStack{ Image("Badge 2")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .opacity(0.3)
+                                            Text("1 Day")
+                                        }
+                                        VStack{ Image("Badge 5")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .opacity(0.3)
+                                            Text("1 Week")
+                                        }
+                                        
+                                        VStack{     Image("Badge 6")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .opacity(0.3)
+                                            Text("1 Month")
+                                        }
+                                    }
+                                    Divider()
+                                    
+                                    //.orientation(.vertical)
+                                    
+                                }
+                                
+                            } .padding(25)
+                            
+                            if month == "Dec" {
+                                Text("2024")
+                                    .font(.title)
+                                    .fontWeight(.semibold)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .padding()
+                                
+                            }
+                        }
+                    }
+                }
             }
-            
         }
     }
-
+}
 
 #Preview {
     BadgesCollectionScreen()
